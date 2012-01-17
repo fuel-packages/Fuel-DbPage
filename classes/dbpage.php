b9a28553f11f16701a510b7516f0c500bf8853f6
Phil-F <info@weztec.com>
2012-01-17 00:03:07 +0000

    Made it more robust when checking segments

--- classes/dbpage.php
+++ classes/dbpage.php
@@ -14,23 +14,23 @@ class DbPage {
     /**
      * @var  cms table name
      */
-    public static $table            = null;
+    public static $table = null;
     /**
      * @var  page id
      */
-    public static $id               = null;
+    public static $id = null;
     /**
      * @var  responce status
      */
-    public static $response_status  = 200;
+    public static $response_status = 200;
     /**
      * @var  page name
      */
-    public static $page             = null;
+    public static $page = null;
     /**
      * @var  page parameters
      */
-    public static $parameters       = null;
+    public static $parameters = null;
     /**
      * @var  page title
      */
@@ -56,23 +56,26 @@ class DbPage {
      * Initialise the class
      *
      * @param
-     * @return	
+     * @return
      */
     public static function _init()
     {
         \Config::load('dbpage', true);
-        
-        static::$table  = \Config::get('dbpage.db.table', 'pages');
-        $installed      = \Config::get('dbpage.db.installed', false);
 
-        if (!$installed)
+        static::$table = \Config::get('dbpage.db.table', 'pages');
+        $installed = \Config::get('dbpage.db.installed', false);
+
+        if( ! $installed)
         {
-            if (!static::_install_db())
+            if( ! static::_install_db())
             {
                 throw new \Exception('Could not create db pages table.');
             }
 
-            \Config::set('dbpage.db', array('table' => static::$table, 'installed' => true));
+            \Config::set('dbpage.db', array(
+                    'table' => static::$table,
+                    'installed' => true
+            ));
             \Config::save('dbpage', \Config::get('dbpage'));
         }
     }
@@ -81,24 +84,49 @@ class DbPage {
      * Store the page to class static vars
      *
      * @param
-     * @return 
+     * @return
      */
     public static function load()
     {
-        //$segments   = explode('/', \Uri::detect(), 2); // Versions prior to 1.1
-        $segments   = explode('/', \Input::uri(), 2);// Version 1.1
-        $page       = (!empty($segments [0])) ? $segments [0] : 'home';
-        $parameters = (!empty($segments [1])) ? $segments [1] : null;
+        $segments = explode('/', \Input::uri(), 3);
+
+        $page = ( ! empty($segments[0])) ? $segments[0] : null;
+        $parameters = ( ! empty($segments[1])) ? $segments[1] : null;
+
+		// If it fails move on a segment
+		if ( ! isset($page))
+		{
+			$page = ( ! empty($segments[1])) ? $segments[1] : 'home';
+			$parameters = ( ! empty($segments[2])) ? $segments[2] : null;
+		}
+
+        $ignore = array(
+                'admin',
+                'assets',
+                'favicon'
+        );
+
+        if(in_array($page, $ignore))
+        {
+            return null;
+        }
+
+        if(substr($page, strlen($page) - 4, 4) === '.php')
+        {
+            $page = substr($page, 0, strlen($page) - 4);
+            \Response::redirect($page, 'refresh');
+        }
 
         $result = \DB::select('*')
                 ->from('pages')
                 ->where('page', $page)
                 ->where('status', true)
                 ->limit(1)
+                ->cached(3600 * 24, $page . '-page')
                 ->execute()
                 ->current();
 
-        if (!$result)
+        if( ! $result)
         {
             static::$response_status = 404;
             $result = \DB::select('*')
@@ -109,14 +137,14 @@ class DbPage {
                     ->current();
         }
 
-        static::$id                 = $result['id'];
-        static::$page               = $page;
-        static::$parameters         = $parameters;
-        static::$meta_title         = $result['meta_title'];
-        static::$meta_keywords      = $result['meta_keywords'];
-        static::$meta_description   = $result['meta_description'];
-        static::$content            = $result['content'];
-        static::$status             = $result['status'];
+        static::$id = $result['id'];
+        static::$page = $page;
+        static::$parameters = $parameters;
+        static::$meta_title = $result['meta_title'];
+        static::$meta_keywords = $result['meta_keywords'];
+        static::$meta_description = $result['meta_description'];
+        static::$content = $result['content'];
+        static::$status = static::$response_status;
     }
 
     /**
@@ -127,7 +155,7 @@ class DbPage {
      */
     public static function add_page_data($page = array())
     {
-        if (sizeof($page > 0))
+        if(sizeof($page > 0))
         {
             $result = \DB::insert(static::$table)
                     ->set($page)
@@ -140,20 +168,19 @@ class DbPage {
     /**
      * Add a page to the database using the class static vars
      *
-     * @param    
+     * @param
      * @return   An array containing a list of insert id and rows created
      */
     public static function add_page()
     {
         $page = array(
-            'page'              => static::$response_status,
-            'meta_title'        => static::$meta_title,
-            'meta_keywords'     => static::$meta_keywords,
-            'meta_description'  => static::$meta_description,
-            'content'           => static::$content,
-            'status'            => static::$status,
+                'page' => static::$response_status,
+                'meta_title' => static::$meta_title,
+                'meta_keywords' => static::$meta_keywords,
+                'meta_description' => static::$meta_description,
+                'content' => static::$content,
+                'status' => static::$status,
         );
-
         $result = \DB::insert(static::$table)
                 ->set($page)
                 ->execute();
@@ -169,7 +196,7 @@ class DbPage {
      */
     public static function update_page_data($id = null, $page = array())
     {
-        if (sizeof($page > 0) and $id > 0)
+        if(sizeof($page > 0) and $id > 0)
         {
             $result = \DB::update(static::$table)
                     ->where('id', $id)
@@ -189,14 +216,13 @@ class DbPage {
     public static function update_page($id = null)
     {
         $page = array(
-            'page'              => static::$response_status,
-            'meta_title'        => static::$meta_title,
-            'meta_keywords'     => static::$meta_keywords,
-            'meta_description'  => static::$meta_description,
-            'content'           => static::$content,
-            'status'            => static::$status,
+                'page' => static::$response_status,
+                'meta_title' => static::$meta_title,
+                'meta_keywords' => static::$meta_keywords,
+                'meta_description' => static::$meta_description,
+                'content' => static::$content,
+                'status' => static::$status,
         );
-
         $result = \DB::insert(static::$table)
                 ->where('id', $id)
                 ->set($page)
@@ -238,30 +264,57 @@ class DbPage {
     /**
      * Automatically create the database with the two required pages home and 404
      *
-     * @param    
-     * @return   
+     * @param
+     * @return
      */
     private static function _install_db()
     {
         $rows = \DBUtil::create_table(static::$table, array(
-                    'id'                => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true),
-                    'page'              => array('constraint' => 255, 'type' => 'varchar', 'null' => false),
-                    'meta_title'        => array('constraint' => 255, 'type' => 'varchar', 'null' => false),
-                    'meta_keywords'     => array('constraint' => 255, 'type' => 'varchar', 'null' => false),
-                    'meta_description'  => array('type' => 'text', 'null' => false),
-                    'content'           => array('type' => 'text', 'null' => false),
-                    'status'            => array('constraint' => 1, 'type' => 'int', 'default' => 0, 'null' => false),
-                        ), array('id'));
+                'id' => array(
+                        'constraint' => 11,
+                        'type' => 'int',
+                        'auto_increment' => true
+                ),
+                'page' => array(
+                        'constraint' => 255,
+                        'type' => 'varchar',
+                        'null' => false
+                ),
+                'meta_title' => array(
+                        'constraint' => 255,
+                        'type' => 'varchar',
+                        'null' => false
+                ),
+                'meta_keywords' => array(
+                        'constraint' => 255,
+                        'type' => 'varchar',
+                        'null' => false
+                ),
+                'meta_description' => array(
+                        'type' => 'text',
+                        'null' => false
+                ),
+                'content' => array(
+                        'type' => 'text',
+                        'null' => false
+                ),
+                'status' => array(
+                        'constraint' => 1,
+                        'type' => 'int',
+                        'default' => 0,
+                        'null' => false
+                ),
+        ), array('id'));
 
-        if ($rows > 0)
+        if($rows > 0)
         {
             $page = array(
-                'page'              => '404',
-                'meta_title'        => 'Page can not be found',
-                'meta_keywords'     => 'Page can not be found',
-                'meta_description'  => 'Page can not be found',
-                'content'           => '<h1>Page can not be found!</h1>',
-                'status'            => 1,
+                    'page' => '404',
+                    'meta_title' => 'Page can not be found',
+                    'meta_keywords' => 'Page can not be found',
+                    'meta_description' => 'Page can not be found',
+                    'content' => '<h1>Page can not be found!</h1>',
+                    'status' => 1,
             );
 
             \DB::insert(static::$table)
@@ -269,14 +322,13 @@ class DbPage {
                     ->execute();
 
             $page = array(
-                'page'              => 'home',
-                'meta_title'        => 'Home Page',
-                'meta_keywords'     => 'Home Page',
-                'meta_description'  => 'Home Page',
-                'content'           => '<h1>Insert your content...</h1>',
-                'status'            => 1,
+                    'page' => 'home',
+                    'meta_title' => 'Home Page',
+                    'meta_keywords' => 'Home Page',
+                    'meta_description' => 'Home Page',
+                    'content' => '<h1>Insert your content...</h1>',
+                    'status' => 1,
             );
-
             \DB::insert(static::$table)
                     ->set($page)
                     ->execute();
